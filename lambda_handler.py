a0f11e4648153a785a7c584eaaf4dc5104962613
Dang Nguyen <dang.nguyen@pureintegration.com>
2023-12-12 21:27:40 -0500

    Update dev pipeline

--- lambda_handler.py
+++ lambda_handler.py
@@ -25,7 +25,7 @@ def lambda_handler(event, context):
         print(e.response['Error']['Message'])
     else:
         new_visitor_count = response['Attributes']['visitor_count']
-        print(f'New visitor count (release by tagging): {new_visitor_count}')
+        print(f'New visitor count (create a gh release v1.0.4-dev ): {new_visitor_count}')
         return {
             'statusCode': 200,
             'body': json.dumps({'visitor_count': int(new_visitor_count)}),
